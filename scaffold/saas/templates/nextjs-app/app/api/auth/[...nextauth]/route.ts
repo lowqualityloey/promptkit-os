12cd7e7ccb3bdb7f85256a9380f7ee076a36871a
Jonell Balanay <73455760+lowqualityloey@users.noreply.github.com>
2026-09-17 02:15:06 +1200

    fix(scaffold): missing nextauth prisma adapter and globals.css

--- scaffold/saas/templates/nextjs-app/app/api/auth/[...nextauth]/route.ts
+++ scaffold/saas/templates/nextjs-app/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,11 @@
 import NextAuth from "next-auth"
 // Example using GitHub, replace with actual providers you need
 import GitHub from "next-auth/providers/github"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import prisma from "../../../../lib/prisma"
 
 export const { handlers: { GET, POST }, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GitHub({
       clientId: process.env.GITHUB_ID,
